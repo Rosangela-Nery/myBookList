f58cb7c0c0c6c299a4a280573c67d5bb9835346f
Rosângela Nery <rosangela2nery@gmail.com>
2023-02-09 15:16:40 -0300

    Configuring global styling

---
+++ src/styles/themes/default.js
@@ -0,0 +1,12 @@
+export const defaultTheme = {
+    white: '#FFF',
+  
+    'purple-800': '#3E075B',
+  
+    'gray-400': '#A6A6A6',
+  
+    'green-300': '#7ED957',
+    'green-400': '#63B142',
+  
+    'orange-200': '#FFBD59',
+  }
