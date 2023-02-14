c6738c705ea22705d280a9adea0bfe0e629f4667
Rosângela Nery <rosangela2nery@gmail.com>
2023-02-14 15:42:27 -0300

    Performing HTTP request

--- src/pages/SearchBook/index.js
+++ src/pages/SearchBook/index.js
@@ -1,81 +1,43 @@
+import { useEffect, useState } from "react";
 import { Footer } from "../../components/Footer";
 import { SearchForm } from "../../components/SearchForm";
 import { BookListComponents, BookRecord, ChoiceButton, InformationField } from "./styles";
 
 export function SearchBook() {
+    const [transactions, setTransactions] = useState([])
+
+    async function loadTransactions() {
+        const response = await fetch('http://localhost:3005/bookTransactions');
+        const data = await response.json();
+
+        setTransactions(data);
+    }
+
+    useEffect(() => {
+        loadTransactions();
+    }, [])
+
     return (
         <BookListComponents>
             <SearchForm />
-            <BookRecord>
-                <img src="https://m.media-amazon.com/images/I/51zhRn-CtUL.jpg" alt=""/>
-                <InformationField>
-                    <div>
-                        <p>Nome: Bird Box</p>
-                        <p>Autor: Josh Malerman</p>
-                        <p>Gênero: Supense</p>
-                    </div>
-                    <ChoiceButton>
-                        <button>Quero ler</button>
-                        <button>Já li</button>
-                    </ChoiceButton>
-                </InformationField>
-            </BookRecord>
-            <BookRecord>
-                <img src="https://m.media-amazon.com/images/I/917oLDiFT+S.jpg" alt=""/>
-                <InformationField>
-                    <div>
-                        <p>Nome: O melhor de Mim</p>
-                        <p>Autor: Nicholas Sparks</p>
-                        <p>Gênero: Romance</p>
-                    </div>
-                    <ChoiceButton>
-                        <button>Quero ler</button>
-                        <button>Já li</button>
-                    </ChoiceButton>
-                </InformationField>
-            </BookRecord>
-            <BookRecord>
-                <img src="https://m.media-amazon.com/images/I/91kAw3CSBzL.jpg" alt=""/>
-                <InformationField>
-                    <div>
-                        <p>Nome: Um Porto Seguro</p>
-                        <p>Autor: Nicholas Sparks</p>
-                        <p>Gênero: Romance</p>
-                    </div>
-                    <ChoiceButton>
-                        <button>Quero ler</button>
-                        <button>Já li</button>
-                    </ChoiceButton>
-                </InformationField>
-            </BookRecord>
-            <BookRecord>
-                <img src="https://m.media-amazon.com/images/I/91MEkXRlNjS.jpg" alt=""/>
-                <InformationField>
-                    <div>
-                        <p>Nome: Uma Longa Jornada</p>
-                        <p>Autor: Nicholas Sparks</p>
-                        <p>Gênero: Romance</p>
-                    </div>
-                    <ChoiceButton>
-                        <button>Quero ler</button>
-                        <button>Já li</button>
-                    </ChoiceButton>
-                </InformationField>
-            </BookRecord>
-            <BookRecord>
-                <img src="https://m.media-amazon.com/images/I/A1s+6nCSdmL.jpg" alt=""/>
-                <InformationField>
-                    <div>
-                        <p>Nome: viagem ao centro da terra</p>
-                        <p>Autor: Jules Verne</p>
-                        <p>Gênero: Ação e aventura</p>
-                    </div>
-                    <ChoiceButton>
-                        <button>Quero ler</button>
-                        <button>Já li</button>
-                    </ChoiceButton>
-                </InformationField>
-            </BookRecord>
+            {transactions.map(transaction => {
+                return (
+                    <BookRecord key={transaction.id}>
+                        <img src={transaction.image} alt=""/>
+                        <InformationField>
+                            <div>
+                                <p>Nome: {transaction.name}</p>
+                                <p>Autor: {transaction.author}</p>
+                                <p>Gênero: {transaction.gender}</p>
+                            </div>
+                            <ChoiceButton>
+                                <button>Quero ler</button>
+                                <button>Já li</button>
+                            </ChoiceButton>
+                        </InformationField>
+                    </BookRecord>
+                )
+            })}
             <Footer />
         </BookListComponents>
     )
